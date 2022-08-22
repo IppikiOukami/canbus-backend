1ba22b4eafec1438598b7d866e89964c611d8bab
IppikiOukami <maciasplayers@gmail.com>
2022-08-22 11:41:21 -0600

    Initial commit/ SpringBoot added with maven dependencies

---
+++ src/main/java/com/TeamNumberOne/canbusbackend/CanbusBackendApplication.java
@@ -0,0 +1,12 @@
+package com.TeamNumberOne.canbusbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanbusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanbusBackendApplication.class, args);
+	}
+}
