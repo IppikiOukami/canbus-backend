4123938b457a23a3556eba81b13c3cfadb3cb2b6
ippikioukami <maciasplayers@gmail.com>
2022-08-22 23:35:53 -0600

    added MVC

---
+++ src/main/java/Model/Package.java
@@ -0,0 +1,13 @@
+package Model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Package {
+    private String ID;
+    private String Source_ID;
+    private String Destination_ID;
+    private String message;
+}
