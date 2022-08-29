0517e90acf0fd33a5032d68127b7ed89ebf1cb35
IppikiOukami <maciasplayers@gmail.com>
2022-08-29 11:49:39 -0600

    Added Model Attributes

--- src/main/java/Model/Packet.java
+++ src/main/java/Model/Packet.java
@@ -1,4 +1,15 @@
 package model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class Packet {
+    private String Frame_START;
+    private String Frame_END;
+    private String control_Field;
+    private String data_Field;
+    private String CRC_Field;
+    private boolean ACK;
 }
--- src/main/java/Model/Project.java
+++ src/main/java/Model/Project.java
@@ -1,4 +1,17 @@
 package model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class Project {
+    private String analystInitials;
+    private int canConnectorID;
+    private int vehicleID;
+    private int baudRate;
+    private String event_name;
+    private String event_date;
+    private String DBC_FILE_NAME = null;
+    private String BLACK_LIST_FILE_NAME = null;
 }
