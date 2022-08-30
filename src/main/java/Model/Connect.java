1db1aeda540dde60ac42a71bb60e8b9a23265ee8
ippikioukami <maciasplayers@gmail.com>
2022-08-30 12:19:05 -0600

    Switched to CSV files per table

--- src/main/java/Model/Connect.java
+++ src/main/java/Model/Connect.java
@@ -3,8 +3,19 @@ package Model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Getter
 @Setter
 public class Connect {
-    private String db_URL = "jdbc:mysql://localhost:3306";
+    private Map<String,String> DB_Table_Paths = new HashMap<String,String>();
+
+    public Connect(){
+        DB_Table_Paths.put("DB_Path","canbus-db.csv");
+        DB_Table_Paths.put("Node_Table","Node_Table.csv");
+        DB_Table_Paths.put("Packet_Table","Packet_Table.csv");
+        DB_Table_Paths.put("Project_Table","Project_Table.csv");
+    }
+
 }
